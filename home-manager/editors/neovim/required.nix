bbdfb5a2878c1e2ee8a66b3dbf5e2c2adbba2220
tiendatAI <tiendatlework@gmail.com>
2024-02-06 23:29:48 +0700

    copy astronvim by nix

--- home-manager/editors/neovim/required.nix
+++ home-manager/editors/neovim/required.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 
 {
+  home.file."~/.config/nvim/lua/user" = {
+    source = ./astronvim_user;
+    recursive = true;
+  };
+  
   home.packages = with pkgs; [
     # config editor
     neovim
