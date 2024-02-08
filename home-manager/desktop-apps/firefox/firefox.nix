88e689a51047e27c411e62deae872670864d42a8
tiendatAI <tiendatlework@gmail.com>
2024-02-08 11:28:44 +0700

    firefox: css

--- home-manager/desktop-apps/firefox/firefox.nix
+++ home-manager/desktop-apps/firefox/firefox.nix
@@ -1,11 +1,12 @@
 { config, ... }:
 let 
-  profile = "cpbibh9c.default"
+  profile = "cpbibh9c.default";
   symlink = config.lib.file.mkOutOfStoreSymlink;
 in {
   # link file 
-  home.file.".mozilla/firefox/${profile}/chrome/userChrome.css" = {
-    source = symlink ./minimalisticfox/userChrome.css;
+  home.file.".mozilla/firefox/${profile}/chrome" = {
+    source = ./chrome;
+    recursive = true;
   };
   
   # Source: https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265
