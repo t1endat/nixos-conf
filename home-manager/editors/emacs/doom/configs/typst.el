2b13c1c39b5cbe5779e54d62c55753384a9238e5
tiendatAI <tiendatlework@gmail.com>
2024-02-11 17:31:03 +0700

    sway: extra config

--- home-manager/editors/emacs/doom/configs/typst.el
+++ home-manager/editors/emacs/doom/configs/typst.el
@@ -4,3 +4,5 @@
 (setq treesit-extra-load-path '("~/.config/emacs/tree-sitter"))
 (setq typst-ts-mode-watch-options "--open")
 
+;; apply grammar and flyspell
+(add-hook 'typst-ts-mode-hook 'flyspell-mode)
--- home-manager/gui/sway/sway.nix
+++ home-manager/gui/sway/sway.nix
@@ -1,13 +1,6 @@
 let
   ROOT = builtins.toString ./.;
   readFile = builtins.readFile;
-
-  mod = "Mod4";
-  lockscreen_lock_after = "300"; # 5min
-  lockscreen_turnoff_after = "10"; # 10sec
-  #TODO: better format
-  lockscreen =
-    "exec swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2";
 in {
   wayland.windowManager.sway = {
     enable = true;
@@ -22,7 +15,7 @@ in {
         "type:pointer".accel_profile = "flat";
       };
       output = { "*".bg = "${ROOT}/wallpapers/aesthetic_deer.jpg fill"; };
-      modifier = "${mod}";
+      modifier = "Mod4";
       terminal = "alacritty";
       bars = [{ command = "waybar"; }];
       gaps = {
@@ -31,51 +24,6 @@ in {
       };
       keybindings = (import ./keybindings.nix);
     };
-    extraConfig = ''
-      # flags for keybindings
-      bindsym --no-repeat ${mod}+1 workspace number 1; exec "echo 1 > /tmp/sovpipe"
-      bindsym --no-repeat ${mod}+2 workspace number 2; exec "echo 1 > /tmp/sovpipe"
-      bindsym --no-repeat ${mod}+3 workspace number 3; exec "echo 1 > /tmp/sovpipe"
-      bindsym --no-repeat ${mod}+4 workspace number 4; exec "echo 1 > /tmp/sovpipe"
-      bindsym --no-repeat ${mod}+5 workspace number 5; exec "echo 1 > /tmp/sovpipe"
-      bindsym --no-repeat ${mod}+6 workspace number 6; exec "echo 1 > /tmp/sovpipe"
-      bindsym --no-repeat ${mod}+7 workspace number 7; exec "echo 1 > /tmp/sovpipe"
-      bindsym --no-repeat ${mod}+8 workspace number 8; exec "echo 1 > /tmp/sovpipe"
-      bindsym --no-repeat ${mod}+9 workspace number 9; exec "echo 1 > /tmp/sovpipe"
-      bindsym --no-repeat ${mod}+0 workspace number 10; exec "echo 1 > /tmp/sovpipe"
-
-      bindsym --release ${mod}+1 exec "echo 0 > /tmp/sovpipe"
-      bindsym --release ${mod}+2 exec "echo 0 > /tmp/sovpipe"
-      bindsym --release ${mod}+3 exec "echo 0 > /tmp/sovpipe"
-      bindsym --release ${mod}+4 exec "echo 0 > /tmp/sovpipe"
-      bindsym --release ${mod}+5 exec "echo 0 > /tmp/sovpipe"
-      bindsym --release ${mod}+6 exec "echo 0 > /tmp/sovpipe"
-      bindsym --release ${mod}+7 exec "echo 0 > /tmp/sovpipe"
-      bindsym --release ${mod}+8 exec "echo 0 > /tmp/sovpipe"
-      bindsym --release ${mod}+9 exec "echo 0 > /tmp/sovpipe"
-      bindsym --release ${mod}+0 exec "echo 0 > /tmp/sovpipe"
-
-      # hide titlebar
-      default_border none
-      default_floating_border none
-      font pango:monospace 0
-      titlebar_padding 1
-      titlebar_border_thickness 0
-
-      # daemon
-      exec_always autotiling # tiling layout
-      exec mako --default-timeout 5000 # notification daemon
-      exec wl-paste --watch cliphist store # cliphist daemon
-      exec wlsunset -l 21.0 -L 105.8 # Hanoi lat/long for wlsunset
-      exec rm -f /tmp/sovpipe && mkfifo /tmp/sovpipe && tail -f /tmp/sovpipe | sov -t 500 # sov with swaywm
-      exec sworkstyle # better workspace
-
-      # lockscreen after period of time
-      exec swayidle -w \
-      timeout ${lockscreen_lock_after} '${lockscreen}' \
-      timeout ${lockscreen_turnoff_after} 'if pgrep swaylock; then swaymsg "output * dpms off"; fi' \
-      resume 'swaymsg "output * dpms on"' \
-      before-sleep '${lockscreen}'
-    '';
+    extraConfig = readFile ./extraConfig;
   };
 }
