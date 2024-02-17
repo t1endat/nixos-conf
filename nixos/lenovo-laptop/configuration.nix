1b86f6b8bb7d46ff4b94dc68eb6c3e99fb34fd01
tiendatAI <tiendatlework@gmail.com>
2024-02-17 18:58:29 +0700

    waybar: default icon font

--- nixos/lenovo-laptop/configuration.nix
+++ nixos/lenovo-laptop/configuration.nix
@@ -87,7 +87,8 @@ in {
       serif = [ "Intel One Mono" ];
       sansSerif = [ "Intel One Mono" ];
       monospace = [ "Intel One Mono" ];
-      emoji = [ "Font Awesome 6 Free" ];
+      # emoji = [ "Font Awesome 6 Free" ];
+      emoji = [ "Symbols Nerd Font" ];
     };
   };
 
