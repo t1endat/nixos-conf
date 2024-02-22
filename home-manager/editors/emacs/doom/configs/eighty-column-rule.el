61c9319777a208945b25ac69b543eae32d37cda0
tiendatAI <tiendatlework@gmail.com>
2024-02-22 18:30:10 +0700

    doom: EightyColumnRule

--- home-manager/editors/emacs/doom/configs/eighty-column-rule.el
+++ home-manager/editors/emacs/doom/configs/eighty-column-rule.el
@@ -1,21 +1,4 @@
 ;;; configs/eighty-column-rule.el -*- lexical-binding: t; -*-
 
-;; source: https://www.emacswiki.org/emacs/EightyColumnRule
-
-(require 'whitespace)
-(setq whitespace-style '(face empty tabs lines-tail trailing))
-(global-whitespace-mode t)
-
-(custom-set-faces
- '(my-tab-face            ((((class color)) (:background "grey10"))) t)
- '(my-trailing-space-face ((((class color)) (:background "gray10"))) t)
- '(my-long-line-face ((((class color)) (:background "gray10"))) t))
-
-(add-hook 'font-lock-mode-hook
-          (function
-           (lambda ()
-             (setq font-lock-keywords
-                   (append font-lock-keywords
-                           '(("\t+" (0 'my-tab-face t))
-                             ("^.\\{81,\\}$" (0 'my-long-line-face t))
-                             ("[ \t]+$"      (0 'my-trailing-space-face t))))))))
+;; source: https://emacs.stackexchange.com/questions/147/how-can-i-get-a-ruler-at-column-80
+(add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
