bf0776ce4c6e1a247c035342fd819cf0a8a44450
tiendatAI <tiendatlework@gmail.com>
2024-03-18 18:40:05 +0700

    sway: swich workspace

--- home-manager/gui/sway/switch_workspaces.sh
+++ home-manager/gui/sway/switch_workspaces.sh
@@ -4,6 +4,14 @@ output_1_name=$(swaymsg -t get_outputs --raw | jq '.[0].name' -r)
 output_2_name=$(swaymsg -t get_outputs --raw | jq '.[1].name' -r)
 workspace_on_1=$(swaymsg -t get_outputs --raw | jq '.[0].current_workspace' -r)
 workspace_on_2=$(swaymsg -t get_outputs --raw | jq '.[1].current_workspace' -r)
+
+# move workspace
+swaymsg workspace number ${workspace_on_1:0:1}
 swaymsg move workspace to output $output_2_name
-swaymsg workspace back_and_forth
+swaymsg workspace number ${workspace_on_2:0:1}
 swaymsg move workspace to output $output_1_name 
+
+# move to main screen (2)
+main_workspace=$(swaymsg -t get_outputs --raw | jq '.[1].current_workspace' -r)
+swaymsg workspace number ${main_workspace:0:1}
+
