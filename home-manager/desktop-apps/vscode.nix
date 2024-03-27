b0a96a6ac7fc870dc664a6b8432060eb584424f3
tiendatAI <tiendatlework@gmail.com>
2024-03-27 18:41:21 +0700

    vscode: for some specific application

--- home-manager/desktop-apps/vscode.nix
+++ home-manager/desktop-apps/vscode.nix
@@ -2,14 +2,20 @@
   # source: https://nixos.wiki/wiki/Visual_Studio_Code
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      # dracula-theme.theme-dracula
-      # vscodevim.vim
-      # yzhang.markdown-all-in-one
-    ];
     # source: https://www.reddit.com/r/NixOS/comments/17cc4d7/electron_apps_on_nixos_nvidia_wayland_hyprland/
     userSettings = { 
       "window.titleBarStyle" = "custom"; 
+      "workbench.colorTheme" = "Catppuccin Mocha";
+      "workbench.iconTheme" = "catppuccin-mocha";
     };
+    extensions = with pkgs.vscode-extensions; [
+      catppuccin.catppuccin-vsc 
+      catppuccin.catppuccin-vsc-icons
+      vscodevim.vim
+      mkhl.direnv
+      ms-vscode.cpptools
+      # platformio.platformio-ide
+      usernamehw.errorlens
+    ];
   };
 }
