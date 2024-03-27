d3e3429fcc0241ce914057f4ea945b0750825b94
tiendatAI <tiendatlework@gmail.com>
2024-03-28 01:06:54 +0700

    not have time to play game

--- nixos/base/desktop-apps.nix
+++ nixos/base/desktop-apps.nix
@@ -4,16 +4,6 @@ let
   session = "${pkgs.sway}/bin/sway";
   # username = "tiendat";
 in {
-  # source: https://nixos.wiki/wiki/Steam#Install
-  # gaming
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall =
-      true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall =
-      true; # Open ports in the firewall for Source Dedicated Server
-  };
-
   # source: https://nixos.wiki/wiki/Virt-manager
   # Virt-manager
   virtualisation.libvirtd.enable = true;
