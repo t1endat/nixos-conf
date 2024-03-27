307ff5e5f8fc1cbf027ef5dc13c8cad0551e0784
tiendatAI <tiendatlework@gmail.com>
2024-03-27 18:00:37 +0700

    vscode: for some specific application

--- home-manager/desktop-apps/vscode.nix
+++ home-manager/desktop-apps/vscode.nix
@@ -7,5 +7,9 @@
       # vscodevim.vim
       # yzhang.markdown-all-in-one
     ];
+    # source: https://www.reddit.com/r/NixOS/comments/17cc4d7/electron_apps_on_nixos_nvidia_wayland_hyprland/
+    userSettings = { 
+      "window.titleBarStyle" = "custom"; 
+    };
   };
 }
