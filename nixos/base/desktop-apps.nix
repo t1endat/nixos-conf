837e32560ad4efd0c90a0767588fee2c9cbf4693
tiendatAI <tiendatlework@gmail.com>
2024-03-30 18:38:41 +0700

    not use zip and rar cmd

--- nixos/base/desktop-apps.nix
+++ nixos/base/desktop-apps.nix
@@ -38,6 +38,7 @@ in {
     enable = true;
     plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
   };
+  # source: https://www.reddit.com/r/NixOS/comments/15sip8b/thunar_archiver_extension_not_working/
   programs.file-roller.enable = true;
 
   # other functionalities for nixos
