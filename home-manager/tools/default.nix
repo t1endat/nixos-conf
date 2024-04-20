5fdc79646c10bc8ec97dac109b4146ca395924ce
tiendatAI <tiendatlework@gmail.com>
2024-04-20 23:50:06 +0700

    not use risc-v

--- home-manager/tools/default.nix
+++ home-manager/tools/default.nix
@@ -5,7 +5,6 @@
     ./embedded-programming.nix
     ./fpga-programming.nix
     ./wasm-programming.nix
-    ./riscv-programming.nix
     ./global.nix
     ./tui.nix
   ];
--- home-manager/tools/riscv-programming.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    ripes # GUI simulator for RISC-V ISA
-  ];
-}
