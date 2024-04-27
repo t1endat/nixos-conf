0e936e1598016813c714dea425ae13b0382129ca
tiendatAI <tiendatlework@gmail.com>
2024-04-27 10:41:55 +0700

    only steam in laptop

--- nixos/base/desktop-apps.nix
+++ nixos/base/desktop-apps.nix
@@ -56,11 +56,4 @@ in {
 
   # source: https://nixos.wiki/wiki/Flatpak
   services.flatpak.enable = true;
-
-  # source: https://nixos.wiki/wiki/Steam#Install 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
 }
