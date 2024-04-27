5c3f6c7165ac3d90b15c2c0044ac9b39bac2d609
tiendatAI <tiendatlework@gmail.com>
2024-04-27 16:08:32 +0700

    pkgs-unstable for rust-analyzer

--- home-manager/tools/global.nix
+++ home-manager/tools/global.nix
@@ -1,9 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 
 {
-  home.packages = with pkgs; [
-    (python311.withPackages (ps: with ps; [ ])) # python global
-    rustup # rust global
-    gcc # cc global
+  home.packages = [
+    # (python311.withPackages (ps: with ps; [ ])) # python global
+    pkgs.python311
+    pkgs.gcc # cc global
+    pkgs.rustc # rust global
+    pkgs.cargo
+    pkgs.clippy
+    pkgs.rustfmt
+    pkgs-unstable.rust-analyzer
   ];
 }
