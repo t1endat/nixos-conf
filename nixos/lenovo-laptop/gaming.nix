30397658eb2891e369a84d4ec2443b841c7fd8ea
t1endat <tiendatlework@gmail.com>
2024-05-12 09:01:07 +0700

    not gamming this time

--- nixos/lenovo-laptop/gaming.nix
+++
@@ -1,8 +0,0 @@
-{
-  # source: https://nixos.wiki/wiki/Steam#Install
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
-}
