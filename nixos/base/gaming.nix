97fa3d4fb5355436da6efe8d745b6d6f20b7c97f
tiendatAI <tiendatlework@gmail.com>
2024-05-13 11:47:24 +0700

    gaming time

---
+++ nixos/base/gaming.nix
@@ -0,0 +1,8 @@
+{
+  # source: https://nixos.wiki/wiki/Steam#Install
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+}
