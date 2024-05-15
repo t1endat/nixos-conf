df9bd10b92d49740b47593ebc249ab959339d494
tiendatAI <tiendatlework@gmail.com>
2024-05-15 14:27:33 +0700

    extra host for some specific website

--- nixos/base/extra-hosts.nix
+++ nixos/base/extra-hosts.nix
@@ -1,13 +1,13 @@
 {
   # source:  https://nixos.org/manual/nixos/stable/#sec-configuration-file
   networking.extraHosts = ''
-    0.0.0.0 animevietsub.day
+    0.0.0.0 animevietsub.fun
     0.0.0.0 kick.com
     0.0.0.0 mangadex.org
     0.0.0.0 mangakakalot.to
     0.0.0.0 truyenqqviet.com
-    0.0.0.0 twitch.tv
-    0.0.0.0 vlr.gg
+    0.0.0.0 www.twitch.tv
+    0.0.0.0 www.vlr.gg
     0.0.0.0 ww8.mangakakalot.tv
   '';
 }
