03d1b1ba377d5087de7dab3ec5e34564df350751
tiendatAI <tiendatlework@gmail.com>
2024-05-15 13:49:05 +0700

    not use udev file offline

--- nixos/base/udev.nix
+++ nixos/base/udev.nix
@@ -1,9 +1,16 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: 
+let
+  platformio-udev = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/platformio/platformio-core/develop/platformio/assets/system/99-platformio-udev.rules";
+    sha256 = "1ihkih39vi48azf40z9qjy3m8whnpa21x7fjw2vrbwy15kzvp8yp";
+  };
+in {
   # source: https://discourse.nixos.org/t/how-to-create-files-in-the-etc-udev-rules-d-directory/11929/5
   services.udev.packages = with pkgs; [
     (pkgs.writeTextFile {
       name = "platformio-udev";
-      text = builtins.readFile ./99-platformio-udev.rules;
+      # text = builtins.readFile ./99-platformio-udev.rules;
+      text = builtins.readFile platformio-udev;
       destination = "/etc/udev/rules.d/99-platformio-udev.rules";
     })
     platformio-core.udev
