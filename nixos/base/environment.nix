7931cbbd22db7df98296b6376d48d1fa76bddbcc
t1endat <tiendatlework@gmail.com>
2024-05-21 02:49:19 +0700

    try to use nh for nix

--- nixos/base/environment.nix
+++ nixos/base/environment.nix
@@ -2,8 +2,5 @@
   environment.sessionVariables = {
     # source: https://nixos.wiki/wiki/Visual_Studio_Code#Wayland
     NIXOS_OZONE_WL = "1";
-
-    # source: https://github.com/vimjoyer/nix-helper-video?tab=readme-ov-file#defining-flake
-    FLAKE = "/home/tiendat/Documents/nix-dev/nixos-conf";
   };
 }
---
+++ nixos/lenovo-laptop/environment.nix
@@ -0,0 +1,6 @@
+{
+  environment.sessionVariables = {
+    # source: https://github.com/vimjoyer/nix-helper-video?tab=readme-ov-file#defining-flake
+    FLAKE = "/home/tiendat/Documents/nix-dev/nixos-conf";
+  };
+}
