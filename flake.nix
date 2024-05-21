1f8e9c1bdd9e89c658c61e5e967ed7a19f2ac47d
tiendatAI <tiendatlework@gmail.com>
2024-05-21 15:06:24 +0700

    add more cachix bin

--- flake.nix
+++ flake.nix
@@ -3,9 +3,15 @@
 
   # caching
   nixConfig = {
-    extra-substituters = [ "https://yazi.cachix.org" ];
+    extra-substituters = [ 
+      "https://nix-community.cachix.org" 
+      "https://yazi.cachix.org" 
+    ];
     extra-trusted-public-keys =
-      [ "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" ];
+      [ 
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" 
+        "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" 
+      ];
   };
 
   inputs = {
