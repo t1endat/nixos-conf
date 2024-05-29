1c40413dc25628016ecc52c801b3ce6ccc5175dc
tiendatAI <tiendatlework@gmail.com>
2024-05-29 07:48:42 +0700

    Temporarily disable some packages

--- home-manager/desktop-apps/obs_studio.nix
+++ home-manager/desktop-apps/obs_studio.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }: {
   # source: https://nixos.wiki/wiki/OBS_Studio
-  programs.obs-studio = {
-    enable = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
-      obs-backgroundremoval
-      obs-pipewire-audio-capture
-    ];
-  };
+  # programs.obs-studio = {
+  #   enable = true;
+  #   plugins = with pkgs.obs-studio-plugins; [
+  #     wlrobs
+  #     obs-backgroundremoval
+  #     obs-pipewire-audio-capture
+  #   ];
+  # };
 }
--- home-manager/tools/embedded.nix
+++ home-manager/tools/embedded.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     qemu # machine emulator
-    platformio # embedded tool
-    openocd # On-Chip Debugging
-    gdb # The GNU Project debugger
-    cargo-binutils # invoke the LLVM tools shipped
-    cargo-generate # tool to generaet a new Rust project
+    # platformio # embedded tool
+    # openocd # On-Chip Debugging
+    # gdb # The GNU Project debugger
+    # cargo-binutils # invoke the LLVM tools shipped
+    # cargo-generate # tool to generaet a new Rust project
   ];
 
   # source: https://nixos.wiki/wiki/Virt-manager
