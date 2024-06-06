439bbbdcd09bdab4b1914e3263962154f5712ac3
t1endat <tiendatlework@gmail.com>
2024-06-06 16:30:52 +0700

    alacritty: update keybindings

--- home-manager/gui/alacritty/alacritty.nix
+++ home-manager/gui/alacritty/alacritty.nix
@@ -1,17 +1,15 @@
 { catppuccin-alacritty, ... }:
 let 
 ROOT = builtins.toString ./.;
-# theme = (builtins.fromTOML 
-#     (builtins.readFile "${catppuccin-alacritty}/catppuccin-mocha.toml"));
 theme = (builtins.readFile "${catppuccin-alacritty}/catppuccin-mocha.toml");
 keybindings = (builtins.readFile "${ROOT}/keybindings.toml");
-b = ''
+config = ''
   ${theme}
   ${keybindings}
 '';
 in {
   programs.alacritty = {
     enable = true;
-    settings = (builtins.fromTOML b);
+    settings = (builtins.fromTOML config);
   };
 }
