6d8aadb7b10e9fca74a7842ca0f5051b2f6c07b8
tiendatAI <tiendatlework@gmail.com>
2024-06-09 09:58:04 +0700

    home-manger: change editor env

--- home-manager/environment.nix
+++ home-manager/environment.nix
@@ -3,7 +3,7 @@
     # source: https://nixos.wiki/wiki/Firefox#Screen_Sharing_under_Wayland
     # only needed for Sway
     XDG_CURRENT_DESKTOP = "sway";
-    EDITOR = "helix";
+    EDITOR = "hx";
     BROWSER = "firefox";
   };
 }
