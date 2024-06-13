33b2e79a6820856059750f00c6c9dc5594589dd7
tiendatAI <tiendatlework@gmail.com>
2024-06-13 08:11:22 +0700

    seem like helix miss something

---
+++ home-manager/editors/neovim/neovim.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  home.file.".config/nvim/lua" = {
+    source = ./astro-nvim/lua;
+    recursive = true;
+  };
+
+  home.packages = with pkgs; [
+    # config editor
+    neovim
+
+    # required packages
+  ];
+}
