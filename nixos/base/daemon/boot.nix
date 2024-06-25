ad4bd635fbc4ddbe78e972eee10e6b58b1f44e59
tiendat <tiendatlework@gmail.com>
2024-06-25 19:05:26 +0700

    change limit boot to 5

--- nixos/base/daemon/boot.nix
+++ nixos/base/daemon/boot.nix
@@ -1,5 +1,5 @@
 { ... }: {
   # source: https://nixos.wiki/wiki/Bootloader#Limiting_amount_of_entries_with_grub_or_systemd-boot
   # limit configurations in boot menu
-  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.systemd-boot.configurationLimit = 5;
 }
