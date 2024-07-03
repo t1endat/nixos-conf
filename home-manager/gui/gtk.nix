8930817dcc714e207c10d7a215510a19b97b5f2c
tiendat <tiendatlework@gmail.com>
2024-07-03 17:26:33 +0700

    gtk theme (catpuccin depricated)

--- home-manager/gui/gtk.nix
+++ home-manager/gui/gtk.nix
@@ -1,13 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   gtk = {
     enable = true;
+    # NOTE: it works!
+    # source: https://stackoverflow.com/questions/38576616/how-to-install-gtk-themes-under-nixos-without-hacky-scripts
     theme = {
-      name = "Catppuccin-Mocha-Standard-Blue-Dark";
-      package = pkgs.catppuccin-gtk.override {
-        accents = [ "blue" ];
-        size = "standard";
-        variant = "mocha";
-      };
+      name = "Materia-dark";
+      package = pkgs.materia-theme;
     };
   };
 }
