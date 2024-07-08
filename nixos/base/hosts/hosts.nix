76ba2b00a3197ec4d442309bb099e26c842fedd3
tiendat <tiendatlework@gmail.com>
2024-07-08 21:59:21 +0700

    workmode hosts

--- nixos/base/hosts/hosts.nix
+++ nixos/base/hosts/hosts.nix
@@ -1,9 +1,11 @@
 let
   ROOT = builtins.toString ./.;
   extra-hosts = (builtins.readFile "${ROOT}/extra-hosts");
+  workmode-hosts = (builtins.readFile "${ROOT}/workmode-hosts");
 in {
   # source:  https://nixos.org/manual/nixos/stable/#sec-configuration-file
   networking.extraHosts = ''
     ${extra-hosts}
+    ${workmode-hosts}
   '';
 }
