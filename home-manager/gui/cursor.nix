2c741bdbc09d98a67627c51793401c5b05558038
tiendat <tiendatlework@gmail.com>
2024-07-08 21:03:35 +0700

    cursor theme

--- home-manager/gui/cursor.nix
+++ home-manager/gui/cursor.nix
@@ -1,14 +1,6 @@
 { ... }: {
-  # source: https://nixos.wiki/wiki/Sway#Cursor_is_too_tiny_on_HiDPI_displays
-  # home.pointerCursor = {
-  #   name = "Adwaita";
-  #   package = pkgs.gnome.adwaita-icon-theme;
-  #   size = 20;
-  #   gtk.enable = true;
-  #   x11 = {
-  #     enable = true;
-  #     defaultCursor = "Adwaita";
-  #   };
-  # };
-  catppuccin.pointerCursor.enable = true;
+  catppuccin.pointerCursor = {
+    enable = true;
+    accent = "dark";
+  };
 }
