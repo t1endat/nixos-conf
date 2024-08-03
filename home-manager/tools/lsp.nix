60350a732bce18cdffb30ac59953e7aa9b2a04b9
tiendat <tiendatlework@gmail.com>
2024-08-03 18:21:36 +0700

    add formatter for nix

--- home-manager/tools/lsp.nix
+++ home-manager/tools/lsp.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     nil # lsp for nix
+    nixfmt-rfc-style # nix formatter
     nodePackages.bash-language-server # lsp fo sh  
     markdown-oxide# lsp for markdown
   ];
